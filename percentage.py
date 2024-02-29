f5c666b492f17a58f58945824da0ad7400865d8c
Almas-Akhter <160632064+Almas-Akhter@users.noreply.github.com>
2024-02-29 17:35:14 +0530

    percentage.py

---
+++ percentage.py
@@ -0,0 +1,6 @@
+maths = int(input("Enter your obtained in maths out of 100: "))
+science_marks = int(input ("Enter your marks obtained in science out of 100: "))
+english_marks=int(input("Enter Your Mark obtained in English out of 100: "))
+total=(maths+science_marks+english_marks)/300*100
+
+print('Total percentage is: ', total)
